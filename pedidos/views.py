9556debbdc3c52fa514a2b0fceb4b70f3e23a231
igor <igor-peres@hotmail.com>
2017-08-18 14:03:10 -0400

    1.0

--- pedidos/views.py
+++ pedidos/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def pedidos(request):
+    if request.method == 'GET' and request.GET.get('pedido') != None:
+        pedido1 = request.GET.get('pedido')
+        if pedido1 == 'acai':
+            return render(request, 'acai.html', {'title':'Acai'})
+        if pedido1 == 'mix':
+            return render(request, 'mix.html', {'title':'Mix'})
+        if pedido1 == 'casadinho':
+            return render(request, 'casadinho.html', {'title':'Casadinho'})
+        return render(request, 'pedidos.html', {'title':'Pedidos'})
     return render(request, 'pedidos.html', {'title':'Pedidos'})
